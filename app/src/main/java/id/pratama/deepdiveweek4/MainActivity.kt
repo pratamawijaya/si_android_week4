c471727c207a187b6f104c1a41ec2d9b84b1bf7e
pratamawijaya <set.mnemonix@gmail.com>
2023-09-06 21:02:54 +0700

    add code

--- app/src/main/java/id/pratama/deepdiveweek4/MainActivity.kt
+++ app/src/main/java/id/pratama/deepdiveweek4/MainActivity.kt
@@ -2,10 +2,34 @@ package id.pratama.deepdiveweek4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import id.pratama.deepdiveweek4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val myVar2 = "variable ku"
+
+    lateinit var btnNotification: ImageView
+    lateinit var btnScanQR: Button
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myVariable = "hello"
+
+        // inisisasi view dari xml, paling dasar,
+        btnNotification = findViewById(R.id.btnNotification)
+        btnScanQR = findViewById(R.id.btnScanQR)
+
+    }
+
+    fun myFunction() {}
+
+    fun myFunctionKedua() {
+
     }
 }
